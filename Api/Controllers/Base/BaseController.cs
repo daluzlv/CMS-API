9d5f16055d5d43c20019358d098631b16db210d5
Lucas da Luz <daluzdev@gmail.com>
2024-12-16 13:19:14 -0300

    refactor: - basecontroller was created to make easier access some comands that some controllers will need

---
+++ Api/Controllers/Base/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using Infrastructure.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Api.Controllers.Base;
+
+[ApiController]
+public class BaseController(UserManager<User> userManager) : ControllerBase
+{
+    private readonly UserManager<User> _userManager = userManager;
+
+    protected async Task<Guid?> GetUserIdAsync()
+    {
+        var email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+        if (email == null) return null;
+
+        var user = await _userManager.FindByEmailAsync(email.Value);
+
+        return Guid.Parse(user!.Id);
+    }
+}
