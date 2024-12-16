db133748e7d39c698667caec9e1e6345a9606066
Lucas da Luz <daluzdev@gmail.com>
2024-12-16 11:41:23 -0300

    feat(repositories): - base repository was created

---
+++ Domain/Interfaces/Repositories/Base/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain.Interfaces.Repositories.Base;
+
+public interface IRepository<T> where T : class
+{
+    Task<List<T>> GetAsync(Expression<Func<T, bool>>? expression);
+    Task<T?> GetByIdAsync(Guid id);
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    Task<bool> CommitAsync(CancellationToken cancellationToken = default);
+}
---
+++ Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Interfaces.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Data.Repositories;
+
+public class BaseRepository<T>(AppDbContext dbContext) : IRepository<T> where T : class
+{
+    private readonly AppDbContext _dbContext = dbContext;
+
+    public async Task<List<T>> GetAsync(Expression<Func<T, bool>>? expression)
+    {
+        var query = _dbContext.Set<T>().AsQueryable();
+        if (expression == null)
+            return await query.ToListAsync();
+
+        return await query.Where(expression).ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id) => await _dbContext.Set<T>().FindAsync(id);
+
+    public void Add(T entity) => _dbContext.Add(entity);
+
+    public void Delete(T entity) => _dbContext.Remove(entity);
+
+    public void Update(T entity) => _dbContext.Update(entity);
+
+    public async Task<bool> CommitAsync(CancellationToken cancellationToken = default) =>
+        await _dbContext.SaveChangesAsync(cancellationToken) > 0;
+}
